345442830413329138026d822b9bb849f9f3c977
osada-yum <osada.yum@gmail.com>
2024-07-15 17:36:56 +0900

    Optimization by dual_latice_locality_tableoptim_near.
    	modified:   src/clock_optim_m.f90

--- src/clock_optim_m.f90
+++ src/clock_optim_m.f90
@@ -15,17 +15,20 @@ module sixclock_periodic_optim_m
   real(real64), parameter :: nall_inv = 1d0 / nall
   real(real64), parameter :: kbt = 0.9d0, beta = 1 / kbt
 
-  integer(int32) :: i1, i2
+  integer(int32) :: i1, i2, i3
   real(real64), parameter :: magne_table(0:mstate-1) = cos([(i1 * pi_state_inv, i1 = 0, mstate - 1)])
-  real(real64), parameter :: magne_diff_table(0:mstate-1, 0:mstate-1) = &
-       & reshape([( (magne_table(i2) - magne_table(i1), i1 = 0, mstate - 1), i2 = 0, mstate - 1)], shape = [mstate, mstate])
+  real(real64), parameter :: energy_table_ru(0:mstate-1, 0:mstate-1, 0:mstate-1) = &
+       & reshape([&
+       & ( &
+       &   ( &
+       &     (- sum(cos((i3 - [i1, i2]) * pi_state_inv)), i1 = 0, mstate - 1), &
+       & i2 = 0, mstate - 1), &
+       & i3 = 0, mstate - 1)], shape = [mstate, mstate, mstate])
   real(real64) :: energy_table(0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1)
   real(real64) :: energy_diff_table(0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1)
   real(real64) :: probability_table(0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1, 0:mstate-1)
 
   integer(int32), allocatable :: sixclock_even(:, :), sixclock_odd(:, :)
-  real(real64) :: magne_density, magne_diff
-  real(real64) :: energy_density, energy_diff
 
   integer(int32), parameter :: nd = 4, up = 1, right = 2, down = 3, left = 4 !> 1: up, 2: right, 3: down, 4: left.
   integer(int64), parameter :: dx(nd, 0:1) = &
@@ -41,51 +44,55 @@ contains
   end subroutine print_version
   !> init_sixclock: Initialize 6-state clock model.
   impure subroutine init_sixclock()
-    integer(int32) :: i1, i2, i3, i4, i5, i6
-    write(error_unit, '(a)') "# "//version
     allocate(sixclock_even(nx / 2, ny))
     allocate(sixclock_odd(nx / 2, ny))
-    energy_table(:, :, :, :, :) = &
-         reshape([ &
-         & ( &
-         &  ( &
-         &    ( &
-         &      ( &
-         &        (- sum(cos((i5 - [i1, i2, i3, i4]) * pi_state_inv)), i1 = 0, mstate - 1), &
-         & i2 = 0, mstate - 1), &
-         & i3 = 0, mstate - 1), &
-         & i4 = 0, mstate - 1), &
-         & i5 = 0, mstate - 1)], shape = [mstate, mstate, mstate, mstate, mstate])
-    ! Ec = - sum(cos((c - [u, r, d, l]) * pi_state_inv))
-    energy_diff_table(:, :, :, :, :, :) = &
-         & reshape([ &
-         & ( ( ( ( ( &
-         &           (energy_table(i1, i2, i3, i4, i6) - energy_table(i1, i2, i3, i4, i5), i1 = 0, mstate - 1), &
-         & i2 = 0, mstate - 1), &
-         & i3 = 0, mstate - 1), &
-         & i4 = 0, mstate - 1), &
-         & i5 = 0, mstate - 1), &
-         & i6 = 0, mstate - 1)], shape = [mstate, mstate, mstate, mstate, mstate, mstate])
-    ! delta_e = energy_table(u, r, d, l, nc) - energy_table(u, r, d, l, c)
-    ! energy_diff_table(u, r, d, l, c, nc) = delta_e
-    probability_table(:, :, :, :, :, :) = &
-         & reshape([ &
-         & ( ( ( ( ( &
-         &           (exp(- beta * energy_diff_table(i1, i2, i3, i4, i5, i6)), i1 = 0, mstate - 1), &
-         & i2 = 0, mstate - 1), &
-         & i3 = 0, mstate - 1), &
-         & i4 = 0, mstate - 1), &
-         & i5 = 0, mstate - 1), &
-         & i6 = 0, mstate - 1)], shape = [mstate, mstate, mstate, mstate, mstate, mstate])
+    call init_tables()
   end subroutine init_sixclock
+  !> init_tables: Initialize `energy_table` and `probability_table`.
+  impure subroutine init_tables()
+    real(real64) :: delta_e
+    integer(int32) :: u, r, d, l, c, nc
+    do c = 0, mstate - 1
+       do l = 0, mstate - 1
+          do d = 0, mstate - 1
+             do r = 0, mstate - 1
+                do u = 0, mstate - 1
+                   energy_table(u, r, d, l, c) = calc_local_energy_local(u, r, d, l, c)
+                end do
+             end do
+          end do
+       end do
+    end do
+    ! write(error_unit, *) energy_table
+    do nc = 0, mstate - 1
+       do c = 0, mstate - 1
+          do l = 0, mstate - 1
+             do d = 0, mstate - 1
+                do r = 0, mstate - 1
+                   do u = 0, mstate - 1
+                      delta_e = energy_table(u, r, d, l, nc) - energy_table(u, r, d, l, c)
+                      energy_diff_table(u, r, d, l, c, nc) = delta_e
+                      if (delta_e <= 0.0d0) then
+                         probability_table(u, r, d, l, c, nc) = 2d0
+                      else
+                         probability_table(u, r, d, l, c, nc) = exp(- beta * delta_e)
+                      end if
+                   end do
+                end do
+             end do
+          end do
+       end do
+    end do
+  contains
+    pure real(real64) function calc_local_energy_local(u, r, d, l, c) result(res)
+      integer(int32), intent(in) :: u, r, d, l, c
+      res = - sum(cos((c - [u, r, d, l]) * pi_state_inv))
+    end function calc_local_energy_local
+  end subroutine init_tables
   !> init_sixclock_order: Set spins with the all-alinged state.
   impure subroutine init_sixclock_order()
     sixclock_even(:, :) = 0_int32
     sixclock_odd(:, :) = 0_int32
-    magne_density = 1.0d0
-    magne_diff = 0.0d0
-    energy_density = -2.0d0
-    energy_diff = 0.0d0
   end subroutine init_sixclock_order
   !> update_metropolis: Update the lattice with 1MCS.
   impure subroutine update_metropolis()
@@ -154,16 +161,12 @@ contains
     if (candidate_states >= mstate) candidate_states = candidate_states - mstate
     prob = probability_table(&
          & sixclock_odd(x, uy), sixclock_odd(x, dy), sixclock_odd(lx, y), sixclock_odd(rx, y), &
-         & sixclock_even(x, y), candidate_states)
+         & sixclock_even(x, y), &
+         & candidate_states)
     if (prob < 1d0) then
        if (.not. (grnd() < prob)) return
     end if
     !> prob >= 1d0 .or. grnd() < prob
-    magne_diff = magne_diff + magne_diff_table(sixclock_even(x, y), candidate_states)
-    energy_diff = energy_diff + &
-         & energy_diff_table(&
-         & sixclock_odd(x, uy), sixclock_odd(x, dy), sixclock_odd(lx, y), sixclock_odd(rx, y), &
-         & sixclock_even(x, y), candidate_states)
     sixclock_even(x, y) = candidate_states
   end subroutine local_flip_even
   !> local_flip_odd: Flip a spin of (x, y) position of lattice in the odd checkerboard.
@@ -182,28 +185,45 @@ contains
     if (candidate_states >= mstate) candidate_states = candidate_states - mstate
     prob = probability_table(&
          & sixclock_even(x, uy), sixclock_even(x, dy), sixclock_even(lx, y), sixclock_even(rx, y), &
-         & sixclock_odd(x, y), candidate_states)
+         & sixclock_odd(x, y), &
+         & candidate_states)
     if (prob < 1d0) then
        if (.not. (grnd() < prob)) return
     end if
     !> prob >= 1d0 .or. grnd() < prob
-    magne_diff = magne_diff + magne_diff_table(sixclock_odd(x, y), candidate_states)
-    energy_diff = energy_diff + &
-         & energy_diff_table(&
-         & sixclock_even(x, uy), sixclock_even(x, dy), sixclock_even(lx, y), sixclock_even(rx, y), &
-         & sixclock_odd(x, y), candidate_states)
     sixclock_odd(x, y) = candidate_states
   end subroutine local_flip_odd
   !> calc_energy: Calculate the energy density.
-  impure real(real64) function calc_energy() result(res)
-    energy_density = energy_density + energy_diff * nall_inv
-    energy_diff = 0d0
-    res = energy_density
+  pure real(real64) function calc_energy() result(res)
+    integer(int64) :: x, y
+    integer(int64) :: rx, uy
+    res = 0d0
+    do y = 1, ny
+       uy = y + 1
+       if (uy > ny) uy = 1_int64
+       do x = 1, nx / 2
+          rx = x + dx(right, iand(y, b'1'))
+          if (rx > nx / 2) rx = 1_int64
+          res = res + energy_table_ru(sixclock_odd(x, uy), sixclock_odd(rx, y), sixclock_even(x, y))
+       end do
+       do x = 1, nx / 2
+          rx = x + dx(right, iand(y + 1, b'1'))
+          if (rx > nx / 2) rx = 1_int64
+          res = res + energy_table_ru(sixclock_even(x, uy), sixclock_even(rx, y), sixclock_odd(x, y))
+       end do
+    end do
+    res = res * nall_inv
   end function calc_energy
   !> calc_magne: Calculate the magnetism density.
-  impure real(real64) function calc_magne() result(res)
-    magne_density = magne_density + magne_diff * nall_inv
-    magne_diff = 0d0
-    res = magne_density
+  pure real(real64) function calc_magne() result(res)
+    integer(int64) :: x, y
+    res = 0d0
+    do y = 1, ny
+       do x = 1, nx / 2
+          res = res + magne_table(sixclock_even(x, y))
+          res = res + magne_table(sixclock_odd(x, y))
+       end do
+    end do
+    res = res * nall_inv
   end function calc_magne
 end module sixclock_periodic_optim_m
